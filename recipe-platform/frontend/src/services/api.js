b7852ab3981aff4fa49b8b2a9b96f0620842daba
Kapilkumar <kapilsk838564@gmail.com>
2026-05-06 10:03:16 +0530

    fixed frontend api url

--- recipe-platform/frontend/src/services/api.js
+++ recipe-platform/frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 /* ================= API INSTANCE ================= */
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://reciple-backend.onrender.com/api",
+  baseURL:  import.meta.env.VITE_API_URL,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
