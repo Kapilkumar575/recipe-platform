19dc0352cb4b6718fdb1dc346eb60808f30e650d
Kapilkumar <kapilsk838564@gmail.com>
2026-05-06 09:05:46 +0530

    fix cors

--- recipe-platform/backend/server.js
+++ recipe-platform/backend/server.js
@@ -4,9 +4,6 @@ import cors from "cors";
 
 import connectDB from "./config/db.js";
 
-import "./models/usermodel.js";
-import "./models/Recipemodel.js";
-
 import authRoutes from "./routes/Authroute.js";
 import recipeRoutes from "./routes/Recipe.js";
 import userRoutes from "./routes/user.js";
@@ -23,6 +20,7 @@ app.use(cors({
     "http://localhost:5173",
     "https://recipe-platform-rose.vercel.app"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
 
@@ -33,14 +31,12 @@ app.use("/api/recipes", recipeRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/upload", uploadRoutes);
 
-app.use("/uploads", express.static("uploads"));
-
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
